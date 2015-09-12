c5ef7a392c55134c83711a7d9ee615083737d8e8
Martin Smith <martin.smifff@blueyonder.co.uk>
2015-09-12 10:03:09 +0100

    Initial Commit

---
+++ Photo-Picker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo-Picker
+//
+//  Created by S Smith on 12/09/2015.
+//  Copyright (c) 2015 M Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
